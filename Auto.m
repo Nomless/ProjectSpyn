da450162f5df4003b195e28ae31b844ae983d150
Mary5554 <marisolcor57@gmail.com>
2024-10-30 09:39:50 -0700

    update

--- Auto.m
+++ Auto.m
@@ -1,26 +1,40 @@
-TOUCH_PORT = 1;
-ULTRASONIC_PORT = 4;
+TOUCH_PORT = 4;
+ULTRASONIC_PORT = 1;
 COLOR_PORT = 3;
 LEFT_DRIVE_MOTOR = 'D';
 RIGHT_DRIVE_MOTOR = 'C';
 ARM_MOTOR = 'B';
 
-DRIVE_SPEED = 50;
-TURN_SPEED = 20;
+% 1 - Black
+% 2 - Blue
+% 3 - Green
+% 4 - Yellow
+% 5 - Red
+% 6 - White
+% 7 - Brown
+STOP_COLOR = 5;
+
+DRIVE_SPEED = 60;
+LEFT_OFFSET = 0;
+TURN_SPEED = 80;
 
 touched = false;
 
 brick.beep();
 
-brick.MoveMotor(LEFT_DRIVE_MOTOR, -DRIVE_SPEED);
-brick.MoveMotor(RIGHT_DRIVE_MOTOR, -DRIVE_SPEED);
+brick.MoveMotor(LEFT_DRIVE_MOTOR, DRIVE_SPEED + LEFT_OFFSET);
+brick.MoveMotor(RIGHT_DRIVE_MOTOR, DRIVE_SPEED);
 
 while 1
+    if isnan(brick.TouchPressed(TOUCH_PORT))
+        disp("NaN???");
+        continue
+    end
     if ~touched && brick.TouchPressed(TOUCH_PORT)
         disp("Touching wall");
         touched = true;
-        brick.MoveMotor(LEFT_DRIVE_MOTOR, DRIVE_SPEED);
-        brick.MoveMotor(RIGHT_DRIVE_MOTOR, DRIVE_SPEED);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, -DRIVE_SPEED);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, -DRIVE_SPEED);
         pause(0.2);
         brick.StopMotor(LEFT_DRIVE_MOTOR);
         brick.StopMotor(RIGHT_DRIVE_MOTOR);
@@ -29,25 +43,34 @@ while 1
     distance = brick.UltrasonicDist(ULTRASONIC_PORT);
     if touched
         % move right
-        brick.MoveMotor(LEFT_DRIVE_MOTOR, -TURN_SPEED);
-        brick.MoveMotor(RIGHT_DRIVE_MOTOR, TURN_SPEED);
-        pause(1.7);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, TURN_SPEED);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, -TURN_SPEED);
+        pause(1.9);
         brick.StopMotor(LEFT_DRIVE_MOTOR);
         brick.StopMotor(RIGHT_DRIVE_MOTOR);
         touched = false;
         pause(0.5);
-        brick.MoveMotor(LEFT_DRIVE_MOTOR, -DRIVE_SPEED);
-        brick.MoveMotor(RIGHT_DRIVE_MOTOR, -DRIVE_SPEED);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, DRIVE_SPEED + LEFT_OFFSET);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, DRIVE_SPEED);
+    elseif brick.ColorCode(COLOR_PORT) == STOP_COLOR
+        brick.StopMotor(LEFT_DRIVE_MOTOR);
+        brick.StopMotor(RIGHT_DRIVE_MOTOR);
+        pause(2);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, DRIVE_SPEED + LEFT_OFFSET);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, DRIVE_SPEED);
+        pause(0.5);
     elseif distance > 40
+        pause(0.25);
         % move left
-        brick.MoveMotor(LEFT_DRIVE_MOTOR, TURN_SPEED);
-        brick.MoveMotor(RIGHT_DRIVE_MOTOR, -TURN_SPEED);
-        pause(1.5);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, -TURN_SPEED);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, TURN_SPEED);
+        pause(1.8);
         brick.StopMotor(LEFT_DRIVE_MOTOR);
         brick.StopMotor(RIGHT_DRIVE_MOTOR);
         pause(0.5);
-        brick.MoveMotor(LEFT_DRIVE_MOTOR, -DRIVE_SPEED);
-        brick.MoveMotor(RIGHT_DRIVE_MOTOR, -DRIVE_SPEED);
+        brick.MoveMotor(LEFT_DRIVE_MOTOR, DRIVE_SPEED + LEFT_OFFSET);
+        brick.MoveMotor(RIGHT_DRIVE_MOTOR, DRIVE_SPEED);
+        pause(2.5);
     end
     
     pause(0.1);
--- Test.m
+++ Test.m
@@ -1,9 +1,13 @@
 
+brick.SetColorMode(3, 2);
 brick.beep();
 
 while 1
+    pressed = brick.TouchPressed(1);
     disp(brick.TouchPressed(1));
-    if brick.TouchPressed(1)
+    if isnan(pressed)
+        disp("nan");
+    elseif pressed
         brick.beep();
     end
     pause(0.25)
