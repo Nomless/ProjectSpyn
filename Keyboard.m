4c09c38bd5b128303df9226d0776ffb5a4605003
Nomless <33536239+Nomless@users.noreply.github.com>
2024-11-13 09:11:44 -0700

    keyboard

---
+++ Keyboard.m
@@ -0,0 +1,49 @@
+global key
+InitKeyboard();
+
+speed = 100;
+
+LEFT_MOTOR = 'D';
+RIGHT_MOTOR = 'C';
+
+while 1
+    pause(0.1);
+    switch key
+        case 'w'
+            disp("Up");
+            brick.MoveMotor(LEFT_MOTOR, -speed);
+            brick.MoveMotor(RIGHT_MOTOR, -speed);
+        case 'a'
+            disp('left');
+            brick.MoveMotor(LEFT_MOTOR, speed);
+            brick.MoveMotor(RIGHT_MOTOR, -speed);
+        case 's'
+            disp("down");
+            brick.MoveMotor(LEFT_MOTOR, speed);
+            brick.MoveMotor(RIGHT_MOTOR, speed);
+        case 'd'
+            disp('right');
+            brick.MoveMotor(LEFT_MOTOR, -speed);
+            brick.MoveMotor(RIGHT_MOTOR, speed);
+        case 'q'
+            disp('right');
+            brick.MoveMotor(LEFT_MOTOR, -speed/2);
+            brick.MoveMotor(RIGHT_MOTOR, -speed);
+        case 'e'
+            disp('right');
+            brick.MoveMotor(LEFT_MOTOR, -speed);
+            brick.MoveMotor(RIGHT_MOTOR, -speed/2);
+        case 'uparrow'
+            speed = speed + 10
+        case 'downarrow'
+            speed = speed - 10
+        case 0
+            brick.StopMotor(LEFT_MOTOR);
+            brick.StopMotor(RIGHT_MOTOR);
+        case 'p'
+            break;
+    end
+end
+brick.StopMotor(LEFT_MOTOR);
+brick.StopMotor(RIGHT_MOTOR);
+CloseKeyboard();
